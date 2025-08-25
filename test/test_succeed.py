5ea515e7eaf7e089eb0193a6539edb94e77900b5
faretek <107722825+FAReTek1@users.noreply.github.com>
2025-08-25 09:19:40 +0100

    pytest doesnt run in test directory

--- test/test_succeed.py
+++ test/test_succeed.py
@@ -3,6 +3,4 @@ import os
 
 def test_succeed():
     assert 1 == 1
-
-    assert os.getcwd().endswith("test/")
-    # assert os.getenv("TEST_SECRET") is not None
+    assert os.getenv("TEST_SECRET") is not None
